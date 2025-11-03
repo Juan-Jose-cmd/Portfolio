7e68ee1738e020a6c7f9c430fa94d8a175a8ee31
Juan Jose Pereyra <juanjopereyra0@gmail.com>
2025-11-03 12:33:11 -0300

    section sobre mi terminada

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 //Domcontent.... espera que cargue el dom
-
 document.addEventListener('DOMContentLoaded', () => {
 
     const header = document.querySelector('header');
     const claymore_1 = document.querySelector('.claymore_1');
     const claymore_2 = document.querySelector('.claymore_2');
     const textoClick = document.querySelector('header p');
+    const sobreMiSection = document.getElementById('sobre-mi');
+    const btnContacto = document.querySelector('.btn-contacto');
 
     let clickRealizado = false;
 
@@ -15,14 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
         clickRealizado = true;
 
-        console.log('Iniciando animacion');
-
         claymore_1.classList.add('salir');
         claymore_2.classList.add('salir');
         textoClick.classList.add('salir');
 
         setTimeout(() => {
             header.classList.add('header-oculto');
+            sobreMiSection.classList.add('aparecer-sobre-mi');
         }, 1800);
     }
 
